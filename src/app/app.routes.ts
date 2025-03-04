7d74c685f7e0e736f48bef8d03b8b42b9d2563ec
MKeys-Capstone <mkeys@capstone-is.com>
2025-03-04 13:09:04 -0500

    clear

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { StepFunctionLoadingIndicatorComponent } from './components/step-function-loading-indicator/step-function-loading-indicator.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: StepFunctionLoadingIndicatorComponent,
+  },
+];
---
+++ src/app/components/step-function-loading-indicator/step-function-loading-indicator.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-step-function-loading-indicator',
+  imports: [],
+  templateUrl: './step-function-loading-indicator.component.html',
+  styleUrl: './step-function-loading-indicator.component.scss'
+})
+export class StepFunctionLoadingIndicatorComponent {
+
+}
